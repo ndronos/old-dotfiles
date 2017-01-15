e26c67972646da6eb0560219b2ac929a3a75cfb5
Leandro Leite <ndronos@gmail.com>
2017-01-15 20:51:35 -0200

    [apt-get] Clean

--- apt-get.sh
+++ apt-get.sh
@@ -4,4 +4,9 @@ sudo apt-get update -y
 echo "Upgrade **********"
 sudo apt-get upgrade -y
 
+echo "Dist-upgrade *****"
+sudo apt-get dist-upgrade -y
+
+echo "Clean ************"
+sudo apt-get clean -y
 
